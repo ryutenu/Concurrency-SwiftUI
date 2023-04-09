dc0640f320b734b5b07b1460aba9950113ec23ab
Alan Liu <ryutenu.tokyo@gmail.com>
2023-04-10 05:51:34 +0900

    [Add] files

---
+++ Concurrency-SwiftUI/Concurrency_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_SwiftUIApp.swift
+//  Concurrency-SwiftUI
+//
+//  Created by Alan Liu on 2023/04/10.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
